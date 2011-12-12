a0cb55513ab17840d4a45b97f79030adadec39ab
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2011-12-12 16:14:34 +0200

    autoload added for TaskManager

--- lib/sshlave.rb
+++ lib/sshlave.rb
@@ -1,6 +1,7 @@
 SSHLAVE_PATH = ENV['SSHLAVE_PATH'] ||= File.expand_path("~/sshlave") unless defined?(SSHLAVE_PATH)
 
 module SSHlave
-  autoload :CLI,      'sshlave/cli.rb'
-  autoload :VERSION,  'sshlave/version.rb'
+  autoload :CLI,          'sshlave/cli.rb'
+  autoload :TaskManager,  'sshlave/task_manager.rb'
+  autoload :VERSION,      'sshlave/version.rb'
 end
