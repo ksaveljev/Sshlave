4005bf96f7af9af0d9f11072deb7e4a5adb7e477
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2011-12-14 12:50:21 +0200

    :list task updated, formatting change

--- lib/sshlave/cli.rb
+++ lib/sshlave/cli.rb
@@ -4,8 +4,6 @@ module SSHlave
 
     def start(*args)
       SSHlave::TaskManager.load_tasks
-      puts "load_tasks completed"
-      puts args.empty? ? "empty" : "not empty"
       args.empty? ? SSHlave::TaskManager.run_task(:help) : SSHlave::TaskManager.run_task(*args)
     rescue
       puts ("Sorry, '%s' was not found, see available tasks:" % args.join(' ').bold).red
