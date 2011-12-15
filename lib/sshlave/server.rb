53fe0324d41d846aa4e0699aa601aafa8b293ffb
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2011-12-15 21:26:27 +0200

    trying net ssh shell

--- lib/sshlave/server.rb
+++ lib/sshlave/server.rb
@@ -1,4 +1,6 @@
 require 'net/ssh'
+require 'net/ssh/shell'
+
 
 module SSHlave
   class Server
@@ -16,69 +18,8 @@ module SSHlave
       @ssh ||= Net::SSH.start(@host, @user, @options)
     end
 
-    def channel
-      return @channel if @channel
-
-      @state = :opening
-      @channel = ssh.open_channel(&method(:open_succeeded))
-      @channel.on_open_failed(&method(:open_failed))
-
-      ssh.loop { opening? }
-
-      puts "OPENED!"
-
-      @channel
-    end
-
-    def closed?
-      state == :closed
-    end
-
-    def open?
-      state == :open
-    end
-
-    def opening?
-      !open? && !closed?
-    end
-
-    def open_succeeded(channel)
-      @state = :pty
-      @channel.request_pty(:modes => { Net::SSH::Connection::Term::ECHO => 0 }, &method(:pty_requested))
-    end
-
-    def open_failed(channel, code, description)
-      @state = :closed
-      raise "Crazy thing! Couldn't open channel for our ssh session"
-    end
-
-    def pty_requested(channel, success)
-      @state = :shell
-      raise "Crazy thing! Couldn't request pty for our ssh session" unless success
-      @channel.send_channel_request("shell", &method(:shell_requested))
-    end
-
-    def shell_requested(channel, success)
-      @state = :initializing
-      raise "Crazy thing! Couldn't request shell for our ssh session" unless success
-      @channel.on_data(&method(:look_for_initialization_done))
-      @channel.send_data "export PS1=; echo #{separator} $?\n"
-      p separator
-    end
-
-    def look_for_initialization_done(channel, data)
-      p data.class
-      p data
-      if data.include?(separator)
-        @state = :open
-      end
-    end
-
-    def separator
-      @separator ||= begin
-        s = Digest::SHA1.hexdigest([ssh.object_id, object_id, Time.now.to_i, Time.now.usec, rand(0xFFFFFFFF)].join(":"))
-        s << Digest::SHA1.hexdigest(s)
-      end
+    def shell
+      @shell ||= ssh.shell
     end
 
     def log(msg, new_line = true)
@@ -101,24 +42,24 @@ module SSHlave
 
       result = ""
 
-      channel.send_data cmd
-      channel.on_data do |c, data|
-        result << data
-
-        unless options[:silent] || options[:hidden]
-          SSHLAVE_LOGGER.print(data)
-          SSHLAVE_LOGGER.flush
-        end
-
-        if options[:input]
-          match = options[:match] || /password/i
-          if data =~ match
-            options[:input] += "\n" if options[:input][-1] != ?\n
-            channel.send_data(options[:input])
-            SSHLAVE_LOGGER.puts(options[:input]) unless options[:silent] || options[:hidden] || data =~ /password/i
+      shell.execute cmd
+      #channel.on_data do |c, data|
+      #  result << data
+#
+#        unless options[:silent] || options[:hidden]
+#          SSHLAVE_LOGGER.print(data)
+#          SSHLAVE_LOGGER.flush
+#        end
+#
+#        if options[:input]
+#          match = options[:match] || /password/i
+#          if data =~ match
+#            options[:input] += "\n" if options[:input][-1] != ?\n
+#            channel.send_data(options[:input])
+##            SSHLAVE_LOGGER.puts(options[:input]) unless options[:silent] || options[:hidden] || data =~ /password/i
           end
-        end
-      end
+#        end
+#      end
 
       ssh.loop
 
--- sshlave.gemspec
+++ sshlave.gemspec
@@ -22,4 +22,5 @@ Gem::Specification.new do |s|
   # s.add_development_dependency "rspec"
   # s.add_runtime_dependency "rest-client"
   s.add_runtime_dependency "smart_colored"
+  s.add_runtime_dependency "net-ssh-shell"
 end
