b9b11f60272195fd2b5b4054322eab275c808bc8
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2011-12-15 16:16:23 +0200

    TaskManager::TaskNotFound in cli

--- lib/sshlave/cli.rb
+++ lib/sshlave/cli.rb
@@ -5,7 +5,7 @@ module SSHlave
     def start(*args)
       SSHlave::TaskManager.load_tasks
       args.empty? ? SSHlave::TaskManager.run_task(:help) : SSHlave::TaskManager.run_task(*args)
-    rescue SSHlave::TaskManager::NotFound
+    rescue SSHlave::TaskManager::TaskNotFound
       puts ("Sorry, '%s' was not found, see available tasks:" % args.join(' ').bold).red
       puts
       SSHlave::TaskManager.run_task(:list)
