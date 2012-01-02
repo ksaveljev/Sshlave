6375d3e9231b1e29afd824dbd9726dcfa75c17d1
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2012-01-02 11:11:45 +0200

    run cmd without shell

--- lib/sshlave/server.rb
+++ lib/sshlave/server.rb
@@ -15,10 +15,6 @@ module SSHlave
       @ssh ||= Net::SSH.start(@host, @user, @options)
     end
 
-    def shell
-      @shell ||= ssh.shell
-    end
-
     def log(msg, new_line = true)
       super(SSHLAVE_LOGGER_FORMAT % [@user, @name, msg], new_line)
     end
@@ -39,31 +35,27 @@ module SSHlave
 
       result = ""
 
-      process = shell.execute! cmd do |p|
-        p.on_output do |p, data|
-          puts data
+      ssh.open_channel do |channel|
+        channel.request_pty if options[:input] || options[:pty]
+        channel.exec cmd
+        channel.on_data do |c, data|
+          result << data
+          unless options[:silent] || options[:hidden]
+            SSHLAVE_LOGGER.print(data)
+            SSHLAVE_LOGGER.flush
+          end
+          if options[:input]
+            match = options[:match] || /password/i
+            if data =~ match
+              options[:input] += "\n" if options[:input][-1] != ?\n
+              channel.send_data(options[:input])
+              SSHLAVE_LOGGER.puts(options[:input]) unless options[:silent] || options[:hidden] || data =~ /password/i
+            end
+          end
         end
       end
 
-      #channel.on_data do |c, data|
-      #  result << data
-#
-#        unless options[:silent] || options[:hidden]
-#          SSHLAVE_LOGGER.print(data)
-#          SSHLAVE_LOGGER.flush
-#        end
-#
-#        if options[:input]
-#          match = options[:match] || /password/i
-#          if data =~ match
-#            options[:input] += "\n" if options[:input][-1] != ?\n
-#            channel.send_data(options[:input])
-#            SSHLAVE_LOGGER.puts(options[:input]) unless options[:silent] || options[:hidden] || data =~ /password/i
-#          end
-#        end
-#      end
-
-      #ssh.loop
+      ssh.loop
 
       result.chomp
     end
